302d00729980e3d99dd0ae08f78af91a7818999e
dandre <obixango@gmail.com>
2015-08-22 06:11:16 +0000

    edit pin model to solve heroku image upload error

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,10 @@ class Pin < ActiveRecord::Base
   
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  
+  attr_accessor :photo_file_name
+  attr_accessor :photo_content_type
+  attr_accessor :photo_file_size
+  attr_accessor :photo_updated_at
+
 end
